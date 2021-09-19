06539ce04cfed4f2b2991a4c2773bb3203c9c620
AutoLogon <msadrolodabaee@myseneca.ca>
2021-09-19 09:03:11 -0400

    UserDetailsServiceImpl

--- src/main/java/com/vote/repositories/UserRepository.java
+++ src/main/java/com/vote/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.vote.repositories;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	void findByUsername(String username);
 
 }
--- src/main/java/com/vote/security/WebSecurityConfiguration.java
+++ src/main/java/com/vote/security/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.vote.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,9 +8,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.core.userdetails.userDetailsService;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	
+	
+	@Autowired
+	private userDetailsService m_userDetailsService;
+	
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
